960167bf66c5d7a0d5439f2eeefebfdc874b1900
Israel-O-John <osuborisrael3@gmail.com>
2026-02-27 10:49:07 +0100

    still initial commit

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -10,10 +10,10 @@ function Hero() {
       <div className="absolute inset-0 bg-blue-700/40"></div>
 
       <div className="relative z-10 flex flex-col items-center justify-center h-full space-y-2 py-8 max-w-6xl mx-auto px-6">
-        <h2 className="uppercase text-xl md:text-3xl text-white font-bold">
+        <h2 className="uppercase text-center text-xl md:text-3xl text-white font-bold">
           Expert in Ac repair and maintenance
         </h2>
-        <p className="text-gray-200 pb-8 md:text-lg ">
+        <p className="text-gray-200 pb-8 md:text-lg text-center">
           Stay cool and comfortable all year round with our expert AC repair and
           maintenace services. <br />
           We're here to keep your air conditioner running at it's best, ensuring
